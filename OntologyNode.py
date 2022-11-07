57616042693cf98407182b24f29a0c8882d40e9d
pastext <cocu.xyu.1@mail.ru>
2022-11-07 03:07:04 +0300

    Красивые методы с выводом в строке значения арг

--- OntologyNode.py
+++ OntologyNode.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from unicodedata import name
+#from unicodedata import name
 
 class SomethingWrong(Exception):
     pass
@@ -38,9 +40,67 @@ class Node:
         print(self.right)
         if self.left is not None:
             self.left.outputting_list()
+
+    #Нормальное добавление в ветку, которая является
+    # правым потомком узла, в котором вызван метод ↵
+    #   ⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓
+    def add_to_list(self, object: str = 'object'):
+        name = input(f'Name of adding  {object}\n')
+        if self.right != None:
+            curr = self.right
+            while (curr.left != None) and (curr.right < name):
+                curr = curr.left
+            if curr.right == name:
+                print('Already exist\n')
+            elif curr.right > name:
+                curr.left = Node(left = curr.left, right = curr.right)
+                curr.right = name
+            else:
+                curr.left = Node(right = name)
+        else:
+            self.right = Node(right = name)
+
+    def add_to_list_unnamed(self, name):#, object: str = 'object'):
+        if self.right != None:
+            curr = self.right
+            while (curr.left != None) and (curr.right < name):
+                curr = curr.left
+            if curr.right == name:
+                print('Already exist\n')
+            elif curr.right > name:
+                curr.left = Node(left = curr.left, right = curr.right)
+                curr.right = name
+            else:
+                curr.left = Node(right = name)
+        else:
+            self.right = Node(right = name)
+    #   ⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑
     
-    def add_to_list(self):
-        name = input('Name of class\n')
+    #Нормальное удаление в ветке, которая является
+    # правым потомком узла, в котором вызван метод ↵
+    #   ⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓⇓    
+    def delete_from_list(self, object: str = 'object'):
+        if self.right != None:
+            name = input(f'Name of deleting {object}\n')
+            prev = self
+            curr = self.right
+            while (curr.left is not None) and (curr.right < name):
+                prev = curr
+                curr = curr.left
+            if curr.right == name:#Нужно ещё удалять из классов, отношений, эл. кл., эл. от.
+                if self != prev:
+                    prev.left = curr.left
+                else:
+                    prev.right = curr.left
+                curr = None
+            else:
+                print('Нет такого')
+        else:
+            print('Удалять нечего')
+    #   ⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑⇑
+   
+    def add_to_list_(self, object: str = 'object'):
+        name = input(f'Name of adding {object}\n')
         if self.left:
             self.adding(self.left, name)
         else:
@@ -57,10 +117,10 @@ class Node:
             that.left = Node(left = next, right = name)
         else:
             next.left = Node(right = name)
-    
-    def delete_from_list(self):
+
+    def delete_from_list_(self, object: str = 'object'):
         if self.left:
-            name = input('Name of class\n')
+            name = input(f'Name of deleting {object}\n')
             that = self
             next = self.left
             while (next.left is not None) and (next.right < name):
@@ -74,48 +134,97 @@ class Node:
         else:
             print('Удалять нечего')
 
+    def add_element_to_class(self):#Только начал
+        name = input('Name of class\n')
+        if self.left:
+            pass
+        else:
+            self.left = Node(right = Node(left = name, right = f'Elements of {name}'))
+            name = input('Name of element\n')
+
+
+
                
 class Root(Node):
-        def __init__(self):
-            self.__make_ontotree__()
-            self.c = self.right
-            self.r = self.left.right
-            self.ce = self.left.left.right
-            self.re = self.left.left.left.right
+    def __init__(self):
+        self.__make_ontotree__()
+        self.elements = self
+        self.classes = self.left
+        self.relations = self.left.left
+        self.classes_elements = self.left.left.left
+        self.relations_elements = self.left.left.left.left
+        #self.__menu__()
 
-        def pr(self):
-            print(self.c.right, self.r.right, self.ce.right, self.re.right)    
+    def pr(self):
+        print(self.elements.right, self.classes.right, 
+        self.relations.right, self.classes_elements.right, 
+        self.relations_elements.right)    
         
-        def __make_ontotree__(self):
-            self.right = Node(right = 'Classes')
-            self.left = Node(right = Node(right = 'Relations'))
-            self.left.left = Node(right = Node(right = 'Class elements'))        
-            self.left.left.left = Node(right = Node(right = 'Relations elements'))
+    def __make_ontotree__(self):
+        self.right = None
+        self.left = Node(
+            left = Node(
+                left = Node(
+                    left = Node()                        
+        )))
 
-        def menu(self):
+    def __make_ontotree_test__(self):
+            self.right = Node(right = 'Elements')
+            self.left = Node(right = Node(right = 'Classes'))
+            self.left.left = Node(right = Node(right = 'Relations'))        
+            self.left.left.left = Node(right = Node(right = 'Classes\' elements'))
+            self.left.left.left.left = Node(right = Node(right = 'Relations\' elements'))
+
+    def menu(self):
             x = -1
             while x != 0:
-                x = int(input("Press:\n[1]For adding something\n[2]For see tree\n[3]For delete something\n"))
+                x = int(input("Press:\n[1]For adding something\n[2]For see tree\n" 
+                "[3]For delete something\n"))
                 if x == 1:
-                    x = int(input(" Press:\n [1]For add class\n [2]For add relation\n"))
+                    x = int(input(" Press:\n [1]For add element\n [2]For add class\n"
+                    " [3]For add relation\n"))
                     if x == 1:
-                        self.c.add_to_list()
+                        self.elements.add_to_list('element')
                     elif x == 2:
-                        self.r.add_to_list()
+                        self.classes.add_to_list('class')
+                    elif x == 3:
+                        self.relations.add_to_list('relation')
                 elif x == 2:
-                    self.c.outputting_list()
-                    self.r.outputting_list()
-                    self.outputting_tree()
+                    self.elements.right.outputting_list() if self.elements.right != None\
+                         else print('Empty')
+                    self.classes.right.outputting_list() if self.classes.right != None\
+                         else print('Empty')
+                    self.relations.right.outputting_list() if self.relations.right != None\
+                         else print('Empty')
+                    #self.outputting_tree()
                 elif x == 3:
-                    x = int(input(" Press:\n [1]For delete class\n [2]For delete relation\n"))
+                    x = int(input(" Press:\n [1]For delete element\n [2]For delete class\n"
+                    " [3]For delete relation\n"))
                     if x == 1:
-                        self.c.delete_from_list()
+                        self.elements.delete_from_list('element')
                     elif x == 2:
-                        self.r.delete_from_list()
+                        self.classes.delete_from_list('class')
+                    elif x == 3:
+                        self.relations.delete_from_list('relation')
+
+    def add_element(self):
+        name
+        self.elements.add_to_list('element')
+        
+
+    def add_element_to_class(self):#Только начал
+        name = input('Name of class\n')
+        if self.classes_elements.right != None:
+            pass
+        else:
+            self.classes_elements.right = Node(right = Node(left = name))
+            #self.classes.add_to_list()
+            name = input('Name of element\n')
+            self.classes_elements.right.right.add_to_list('element')#тут будет функция с вводом названия элемента
+            #self.elements.add_to_list()
 
 
 
 tree = Root()
-tree.pr()
+#tree.pr()
 tree.menu()
-#print(None == "aaaa")
