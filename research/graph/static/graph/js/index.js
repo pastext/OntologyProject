09e72bda5ae928d4bf0005a370ce18d533f3b569
manCREEPman <66115736+manCREEPman@users.noreply.github.com>
2023-07-14 00:17:05 +0300

    Initial

---
+++ research/graph/static/graph/js/index.js
@@ -0,0 +1,48 @@
+const container = document.getElementById('container')
+
+// nodes = [
+//     {x: 1, y: 1, r: 4},
+//     {x: 2, y: 2, r: 3},
+//     {x: 3, y: 3, r: 2},
+// ]
+
+// edges = [
+//     {source: 0, target: 1},
+//     {source: 1, target: 2},
+// ]
+
+// Declare the chart dimensions and margins.
+const width = 640;
+const height = 400;
+const marginTop = 20;
+const marginRight = 20;
+const marginBottom = 30;
+const marginLeft = 40;
+
+// Declare the x (horizontal position) scale.
+const x = d3.scaleUtc()
+    .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+    .range([marginLeft, width - marginRight]);
+
+// Declare the y (vertical position) scale.
+const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - marginBottom, marginTop]);
+
+// Create the SVG container.
+const svg = d3.create("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// Add the x-axis.
+svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x));
+
+// Add the y-axis.
+svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y));
+
+// Append the SVG element.
+container.append(svg.node());
